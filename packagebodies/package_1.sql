c5a66290838bbda2aae0c1b894fb52eae7fea54b
th3ALPHAnerd <p1ck37t@gmail.com>
2017-07-06 21:26:41 -0500

    Corrected error with package_1.

--- packagebodies/package_1.sql
+++ packagebodies/package_1.sql
@@ -19,7 +19,7 @@ CREATE OR REPLACE PACKAGE BODY package_1 AS
     p3 VARCHAR2 (32) := 'Hello procedure_3';
   BEGIN
     DBMS_OUTPUT.put_line (num);
-    DBMS_OUTPUT.put_line (p2);
+    DBMS_OUTPUT.put_line (p3);
   END procedure_3;
 
 END package_1;
@@ -27,6 +27,7 @@ END package_1;
 begin
   package_1.procedure_1;
   package_1.procedure_2;
+  package_1.procedure_3;
 end;
 
 /
